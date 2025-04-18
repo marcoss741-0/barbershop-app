d994b9b6fbc061a2f5a5972b36567cb5974ec186
marcoss741-0 <marcossn741@icloud.com>
2025-04-18 11:10:35 -0300

    chore: implement SidebarSheet with login dialog and update dependencies and auth on google

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,14 +1,22 @@
 import {
-  Avatar,
-  AvatarImage,
-  AvatarFallback,
-} from "@/app/_components/ui/avatar";
-import { HomeIcon, CalendarDaysIcon, LogOutIcon } from "lucide-react";
+  HomeIcon,
+  CalendarDaysIcon,
+  LogOutIcon,
+  LogInIcon,
+} from "lucide-react";
 import { ShortSearchOptions } from "../_constants/short-search";
 import { Button } from "./ui/button";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import Link from "next/link";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const SidebarSheet = () => {
   return (
@@ -17,8 +25,38 @@ const SidebarSheet = () => {
         <SheetHeader>
           <SheetTitle className="text-left">Menu</SheetTitle>
         </SheetHeader>
-        <div className="flex items-center gap-3 border-b border-solid py-5">
-          <Avatar className="h-10 w-10">
+        <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+          <h2 className="text-[16px] font-bold">Olá, Faça seu login!</h2>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size="icon" className="cursor-pointer">
+                <LogInIcon size={18} />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="w-[90%] rounded-xl">
+              <DialogHeader>
+                <DialogTitle>Faça login na plataforma</DialogTitle>
+                <DialogDescription>
+                  Conecte-se usando sua conta do Google.
+                </DialogDescription>
+              </DialogHeader>
+              <Button
+                className="items-center gap-2 text-[14px] font-bold"
+                variant="outline"
+              >
+                <Image
+                  src={"/google.svg"}
+                  alt={"Login com o Google"}
+                  width={16}
+                  height={16}
+                />
+                Google
+              </Button>
+            </DialogContent>
+          </Dialog>
+
+          {/* <Avatar className="h-10 w-10">
             <AvatarImage
               src="https://github.com/marcoss741-0.png"
               className="h-10 w-10"
@@ -28,7 +66,7 @@ const SidebarSheet = () => {
           <div className="flex flex-col">
             <p className="text-[16px] font-bold">Marcos Martins</p>
             <p className="text-xs font-normal">marcossan741@icloud.com</p>
-          </div>
+          </div> */}
         </div>
 
         <div className="flex flex-col gap-2 border-b border-solid py-5">
