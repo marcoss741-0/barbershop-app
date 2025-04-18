8fae6ab02a0e87e258c575a172f81591b48021a5
marcoss741-0 <marcossn741@icloud.com>
2025-04-17 21:30:52 -0300

    feat: implement Sheet component and integrate into Header for menu functionality

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,6 +1,21 @@
-import { MenuIcon, ScissorsIcon } from "lucide-react";
+import {
+  CalendarDaysIcon,
+  HomeIcon,
+  LogOutIcon,
+  MenuIcon,
+  ScissorsIcon,
+} from "lucide-react";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import Image from "next/image";
+import { ShortSearchOptions } from "../_constants/short-search";
 
 const Header = () => {
   return (
@@ -14,9 +29,64 @@ const Header = () => {
             </h1>
           </div>
           <div>
-            <Button size={"icon"} variant="outline" className="cursor-pointer">
-              <MenuIcon className="h-5 w-5" />
-            </Button>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button
+                  size={"icon"}
+                  variant="outline"
+                  className="cursor-pointer"
+                >
+                  <MenuIcon className="h-5 w-5" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle className="text-left">Menu</SheetTitle>
+                </SheetHeader>
+                <div className="flex flex-col gap-2 border-b border-solid py-5">
+                  <Button size="sm" className="justify-start gap-2">
+                    <HomeIcon />
+                    Inicio
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    className="justify-start gap-2"
+                  >
+                    <CalendarDaysIcon />
+                    Agendamento
+                  </Button>
+                </div>
+
+                <div className="flex flex-col gap-2 border-b border-solid py-5">
+                  {ShortSearchOptions.map((option) => (
+                    <Button
+                      size="sm"
+                      variant="ghost"
+                      className="justify-start gap-3 p-3"
+                    >
+                      <Image
+                        src={option.iconUrl}
+                        alt={option.title}
+                        width={16}
+                        height={16}
+                      />
+                      {option.title}
+                    </Button>
+                  ))}
+                </div>
+                <div className="flex w-full items-center justify-start gap-2 py-5">
+                  <Button
+                    size="default"
+                    variant="destructive"
+                    className="w-full justify-start gap-3 p-3"
+                  >
+                    <LogOutIcon />
+                    <p className="text-sm">Sair da Conta</p>
+                  </Button>
+                </div>
+              </SheetContent>
+            </Sheet>
           </div>
         </CardContent>
       </Card>
